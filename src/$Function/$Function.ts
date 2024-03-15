058abc37d0883177853d180ff632e50c20d948a8
Frank-styding <frankparedesalpi@gmail.com>
2024-03-15 18:56:17 -0500

    remake

--- src/$Function/$Function.ts
+++ src/$Function/$Function.ts
@@ -17,7 +17,6 @@ import { GlobalStyle } from '../GlobalStyle/GlobalStyle'
 type MapStateValues<T extends State<any>[]> = {
   [K in keyof T]: T[K]['value']
 }
-type Query = `query:${string}`
 type QueryAll = `queryAll:${string}`
 
 // create element
@@ -26,14 +25,12 @@ export function $<T extends keyof ITagMap>(
   b?: ICreateConfig,
   c?: TemplateData[]
 ): Template<ITagMap[T]>
-export function $(a: Query, b?: ICreateConfig, c?: TemplateData[]): Template
 
 export function $<T extends keyof ITagMap>(
   a: T,
   b?: TemplateData[],
   c?: undefined
 ): Template<ITagMap[T]>
-export function $(a: Query, b?: TemplateData[], c?: undefined): Template
 
 export function $<T extends keyof ITagMap>(
   a: T,
@@ -47,14 +44,7 @@ export function $(
   c?: undefined
 ): Template
 
-export function $(a: QueryAll, b?: undefined, c?: undefined): Template[]
 //
-
-export function $<T extends any[]>(
-  a: `component:${string}`,
-  callback: (...value: T) => Template,
-  b?: undefined
-): (...value: T) => Component
 
 export function $<T extends State<any>[]>(
   a: T,
@@ -62,66 +52,11 @@ export function $<T extends State<any>[]>(
   c?: undefined
 ): DynamicTemplate
 
-export function $(a: 'styled', b: string, c?: undefined): typeof create
-
 export function $(a: any, b: any, c: any): any {
   if (Array.isArray(a)) {
     return new DynamicTemplate(a, b)
   }
   if (typeof a === 'string') {
-    if (a.startsWith('component:')) {
-      const componentName = a.replace('component:', '').replace(/ /g, '')
-      const callback = b as (...value: any[]) => Template
-      const componentCallback = (...data: any[]): Component => {
-        const template = callback(...data)
-        return new Component(template, componentName)
-      }
-      return componentCallback
-    }
-
-    if (a.startsWith('query:')) {
-      const element = document.querySelector(
-        a.replace('query:', '').replace(/ /g, '')
-      ) as null | IElement
-      if (element == null) {
-        console.error(`can't find ${a.replace('query:', '').replace(/ /g, '')} query selector`)
-        return undefined
-      }
-
-      if (Array.isArray(b)) {
-        return new Template({ element, childs: b })
-      } else if (typeof b == 'object') {
-        if (Array.isArray(c)) {
-          return new Template({ element, childs: c, ...b })
-        }
-        if (c === undefined) {
-          return new Template({ element, ...b })
-        }
-      } else if (typeof b == 'string') {
-        return new Template({ element, innerHTML: b })
-      } else {
-        return new Template({ element })
-      }
-    }
-
-    if (a.startsWith('queryAll:')) {
-      const templates: Template[] = []
-      const elements = document.querySelectorAll(a.replace('queryAll:', '').replace(/ /g, ''))
-      elements.forEach((element) => {
-        templates.push(new Template({ element: element as IElement }))
-      })
-      return templates
-    }
-
-    if (a == 'styled') {
-      const globalStyle = GlobalStyle.createStyleSelector(b)
-      return (a: any, b: any, c: any) => {
-        const template = create(a, b, c)
-        template.classNames.push(globalStyle)
-        return template
-      }
-    }
-
     if (Array.isArray(b)) {
       return Template.create({ tag: a as keyof ITagMap, childs: b })
     }
@@ -180,7 +115,55 @@ export function create(a: any, b: any, c: any): any {
   return undefined
 }
 
-$.root = Template.Root
+function createState<T>(value: T, data?: undefined): State<T>
+function createState(value: number, data?: ISpringConfig): SpringStateValue
+function createState(a: any, b: any): any {
+  if (b != undefined) {
+    return new SpringStateValue(a, b)
+  }
+  return new State(a)
+}
+$.State = createState
+
+type Query = `query:${string}`
+function query(a: Query, b?: ICreateConfig, c?: TemplateData[]): Template
+function query(a: Query, b?: TemplateData[], c?: undefined): Template
+function query(a: any, b: any, c: any): undefined | Template {
+  const element = document.querySelector(
+    a.replace('query:', '').replace(/ /g, '')
+  ) as null | IElement
+  if (element == null) {
+    console.error(`can't find ${a.replace('query:', '').replace(/ /g, '')} query selector`)
+    return undefined
+  }
+
+  if (Array.isArray(b)) {
+    return new Template({ element, childs: b })
+  } else if (typeof b == 'object') {
+    if (Array.isArray(c)) {
+      return new Template({ element, childs: c, ...b })
+    }
+    if (c === undefined) {
+      return new Template({ element, ...b })
+    }
+  } else if (typeof b == 'string') {
+    return new Template({ element, innerHTML: b })
+  } else {
+    return new Template({ element })
+  }
+}
+$.query = query
+
+$.component = <T extends any[]>(
+  name: string,
+  callback: (...value: T) => Template
+): ((...value: T) => Component) => {
+  const componentCallback = (...data: any[]): Component => {
+    const template = callback(...(data as T))
+    return new Component(template, name)
+  }
+  return componentCallback
+}
 
 $.listen = <T extends State<any>[]>(
   name: string,
@@ -190,13 +173,23 @@ $.listen = <T extends State<any>[]>(
   ListenUpdate(name, b, a)
 }
 
-function createState<T>(value: T, data?: undefined): State<T>
-function createState(value: number, data?: ISpringConfig): SpringStateValue
-function createState(a: any, b: any): any {
-  if (b != undefined) {
-    return new SpringStateValue(a, b)
-  }
-  return new State(a)
+$.queryAll = (a: QueryAll): Template[] => {
+  const templates: Template[] = []
+  const elements = document.querySelectorAll(a.replace('queryAll:', '').replace(/ /g, ''))
+  elements.forEach((element) => {
+    templates.push(new Template({ element: element as IElement }))
+  })
+  return templates
 }
 
-$.State = createState
+$.styled = (b: string): typeof create => {
+  const globalStyle = GlobalStyle.createStyleSelector(b)
+  return (a: any, b: any, c: any) => {
+    const template = create(a, b, c)
+    template.classNames.push(globalStyle)
+    return template
+  }
+}
+
+$.root = Template.Root
+$.createSelector = GlobalStyle.createStyleSelector
